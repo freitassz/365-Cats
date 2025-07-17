1623bec3457e68f0d2a3bf6bac0663e6913cec15
Gustavo de Freitas Bezerra <Gustavobezerraf@gmail.com>
2025-07-17 12:34:55 -0300

    Revert "Jogo inteiro incompleto"
    This reverts commit afac12810441c08cc7feb1f61024b435b88dcb2c.

--- Project/Script/cat_bowl.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-
-func _process(delta: float) -> void:
-	if Global.upgrade_1 == true:
-		self.visible = true
-	else:
-		self.visible = false
--- cat_sandbox.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-
-func _process(delta: float) -> void:
-	if Global.upgrade_4 == true:
-		self.visible = true
-	else:
-		self.visible = false
